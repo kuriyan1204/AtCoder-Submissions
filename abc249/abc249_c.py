70de9814dc325f43832705e22818d6a374327b03
AutoCloneBot <acautoclone@bot.com>
2022-07-29 14:52:58 +0000

    Added my codes from AtCoder

--- abc249/abc249_c.py
+++ abc249/abc249_c.py
@@ -1,12 +1,16 @@
 from collections import Counter, defaultdict
 import string
 
-n, k = map(int, input().split())
+# N, Aを標準入力から受け取る
+n, a = map(int, input().split())
 
+# 標準入力から受け取った文字列を保存する配列
 s = []
 
+# 標準入力から文字列をn個受け取る
 for i in range(n):
-    s.append(defaultdict(int, Counter(input())))
+    #s.append(defaultdict(int, Counter(input())))
+    s.append(Counter(input()))
 
 ans = 0
 # Iterate over bit
@@ -19,7 +23,7 @@ for i in range(2 ** n):
                 tmp[c] += s[j][c]
     tmp_set = set()
     for c in list(string.ascii_lowercase):
-        if tmp[c] == k:
+        if tmp[c] == a:
             tmp_set.add(c)
     ans = max(len(tmp_set), ans)
 print(ans)
